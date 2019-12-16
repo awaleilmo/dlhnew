2fc3544b3a3eb44188aad6ded0b409080bf84510
Awaludin Ramdani <50146172+awaleilmo@users.noreply.github.com>
2019-12-16 19:21:47 +0700

    neee

--- resources/views/strukturbidang/uptlab.blade.php
+++ resources/views/strukturbidang/uptlab.blade.php
@@ -5,68 +5,8 @@
         <div class="container">
             <h2 class="hero-heading wow fadeInDown animated animated" data-wow-duration="0.6s" style="visibility: visible; animation-duration: 0.6s;">UPTD Laboratorium Lingkungan</h2>
             <table border="0" cellpadding="10" class="hero-text wow bounceInUp animated animated" data-wow-duration="0.9s" data-wow-delay="0.2s" style="opacity:1; visibility: visible; animation-duration: 0.9s; animation-delay: 0.2s;">
-                <tr>
-                <td valign="top"><strong>(1)</strong></td>
-                <td align="left">
-                <p style="text-align: justify">Kepala UPTD Laboratorium Lingkungan mempunyai tugas memimpin, merencanakan, membina, mengatur, mengkoordinasikan, dan mengendalikan pelaksanaan tugas UPTD Laboratorium Lingkungan.</p></td>
-                </tr>
+                <br>
 
-                <tr>
-                <td valign="top"><strong>(2)</strong></td>
-                <td align="left">
-                <p style="text-align: justify">Untuk melaksanakan tugas Kepala UPTD Laboratorium Lingkungan mempunyai fungsi :</p></td>
-                </tr>
-
-                <tr>
-                <td valign="top"><strong>a.</strong></td>
-                <td align="left">
-                <p style="text-align: justify"><Merencanakan program dan kegiatan UPTD berdasarkan ketentuan Peraturan Perundang-undangan dan sumber data yang tersedia sebagai pedoman pelaksanaan kegiatan/p></td>
-                </tr>
-
-                <tr>
-                <td valign="top"><strong>b.</strong></td>
-                <td align="left">
-                <p style="text-align: justify">Memberi petunjuk kepada Pelaksana/Pejabat Fungsional/Bawahan sesuai bidang tugas jabatannya dalam rangka pelayanan pemeriksaan di bidang sampling dan analisa kualitas
-                air sungai sampling dan analisa kualitas air limbah, sampling dan analisa kualitas udara ambient, sampling dan analisa kualitas udara kebauan, sampling dan analisa kualitas udara lingkungan kerja, sampling dan analisa kualitas emisi cerobong, sampling dan analisa kebisingan, sampling dan analisa air laut dan/atau bidang lain yang berkaitan dengan lingkungan hidup</p></td>
-                </tr>
-
-                <tr>
-                <td valign="top"><strong>c.</strong></td>
-                <td align="left">
-                <p style="text-align: justify">Memberi petunjuk kepada Pelaksana/Pejabat Fungsional/Bawahan sesuai bidang tugas jabatannya dalam rangka pelayanan pemeriksaan di bidang mikrobiologi, fisika, kimia dan/atau bidang lain yang berkaitan dengan kualitas lingkungan hidup</p></td>
-                </tr>
-
-                <tr>
-                <td valign="top"><strong>d.</strong></td>
-                <td align="left">
-                <p style="text-align: justify">Memberi petunjuk kepada Pelaksana/Pejabat Fungsional/Bawahan sesuai bidang tugas jabatannya dalam rangka layanan informasi mengenai hal-hal yang berkaitan dengan pemeriksaan laboratorium yang akan dilaksanakan</p></td>
-                </tr>
-
-                 <tr>
-                 <td valign="top"><strong>e.</strong></td>
-                 <td align="left">
-                 <p style="text-align: justify">Memberi petunjuk kepada Pelaksana/Pejabat Fungsional/Bawahan sesuai bidang tugas jabatannya dalam rangka pencatatan dan pelaporan mengenai pelaksanaan kegiatan laboratorium</p></td>
-                 </tr>
-
-                 <tr>
-                 <td valign="top"><strong>e.</strong></td>
-                 <td align="left">
-                 <p style="text-align: justify">Untuk melaksanakan tugas Kepala UPTD Laboratorium Lingkungan mempunyai fungsi :</p></td>
-                 </tr>
-
-                 <tr>
-                 <td valign="top"><strong>e.</strong></td>
-                 <td align="left">
-                 <p style="text-align: justify">Untuk melaksanakan tugas Kepala UPTD Laboratorium Lingkungan mempunyai fungsi :</p></td>
-                 </tr>
-
-
-
-                <tr>
-                <td valign="top"></td>
-                <td align="left">
-                <p style="text-align: justify">Berdasarkan Peraturan Walikota Nomor 27 Tahun 2019 tentang pembentukan, kedudukan, susunan organisasi, tugas dan fungsi serta tata kerja unit pelaksana teknis daerah laboratorium lingkungan pada Dinas Lingkungan Hidup Kota Serang</p></td>
-                </tr>
                 <tr>
                     <td align="center"></td>
                     <td><h2 class="hero-heading wow fadeInDown animated animated" data-wow-duration="0.6s" style="visibility: visible; animation-duration: 0.6s;">Dasar Hukum</h2></td>
@@ -78,6 +18,91 @@
                         <p style="text-align: justify">Berdasarkan Peraturan Walikota Nomor 28 Tahun 2019 tentang pembentukan, kedudukan, susunan organisasi, tugas dan fungsi serta tata kerja unit pelaksana teknis daerah laboratorium lingkungan pada Dinas Lingkungan Hidup Kota Serang</p></td>
 
                 </tr>
+
+                <tr>
+                    <td align="center"></td>
+                    <td><h2 class="hero-heading wow fadeInDown animated animated" data-wow-duration="0.6s" style="visibility: visible; animation-duration: 0.6s;">Tugas Pokok dan Fungsi UPTD Laboratorium</h2></td>
+                </tr>
+                <tr>
+                <td valign="top"><strong>(1)</strong></td>
+                <td align="left">
+                <p style="text-align: justify">UPTD Laboratorium Lingkungan sebagaimana dimaksud dalam Pasal 2 mempunyai tugas menyelenggarakan sebagian tugas Dinas yang berkenaan dengan Laboratorium Lingkungan.</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>(2)</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Untuk melaksanakan Tugas, UPTD Laboratorium Lingkungan mempunyai Fungsi :</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>a.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa kualitas air sungai</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>b.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa kualitas air limbah</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>c.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa kualitas sedimen.</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>d.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa kualitas tanah.</p></td>
+                </tr>
+
+                 <tr>
+                 <td valign="top"><strong>e.</strong></td>
+                 <td align="left">
+                 <p style="text-align: justify">Sampling dan analisa kualitas udara ambient.</p></td>
+                 </tr>
+
+                 <tr>
+                 <td valign="top"><strong>f.</strong></td>
+                 <td align="left">
+                 <p style="text-align: justify">Sampling dan analisa kualitas udara kebauan</p></td>
+                 </tr>
+
+                 <tr>
+                 <td valign="top"><strong>g.</strong></td>
+                 <td align="left">
+                 <p style="text-align: justify">Sampling dan analisa kualitas udara lingkungan kerja.</p></td>
+                 </tr>
+
+                <tr>
+                <td valign="top"><strong>h.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa kualitas emisi cerobong.</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>i.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa kebisingan.</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>j.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Sampling dan analisa air laut</p></td>
+                </tr>
+
+                <tr>
+                <td valign="top"><strong>k.</strong></td>
+                <td align="left">
+                <p style="text-align: justify">Pelaporan</p></td>
+                </tr>
+
+
+
                 <br>
                 <br>
                 <br>
@@ -100,11 +125,6 @@
                 <center><img src="image-resources/dlh/sopsample.png" style="width: 30em; border-radius: 30px">
                 </center>
                 </td>
-                </tr>
-                <tr>
-                    <td valign="top"><strong>2.</strong></td>
-                    <td align="left">
-                        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p></td>
                 </tr>
             </table>
 
--- resources/views/strukturbidang/uptperbekalan.blade.php
+++ resources/views/strukturbidang/uptperbekalan.blade.php
@@ -3,11 +3,84 @@
 @section('content')
     <div class="hero-box bg-white hero-box-smaller ">
         <div class="container">
-            <h2 class="hero-heading wow fadeInDown animated animated" data-wow-duration="0.6s" style="visibility: visible; animation-duration: 0.6s;">UPT Perbekalan</h2>
+            <h2 class="hero-heading wow fadeInDown animated animated" data-wow-duration="0.6s" style="visibility: visible; animation-duration: 0.6s;">UPTD Perbekalan</h2>
             <table border="0" cellpadding="10" class="hero-text wow bounceInUp animated animated" data-wow-duration="0.9s" data-wow-delay="0.2s" style="visibility: visible; animation-duration: 0.9s; animation-delay: 0.2s;">
                 <br>
                 <br>
                 <br>
+
+                <tr>
+                    <td valign="top"><strong>(1)</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">UPTD Perlengkapan dan Perbekalan mempunyai tugas menyelenggarakan sebagian tugas Dinas yang berkenaan dengan pengelolaan, pengoperasian, pemeliharaan peralatan mekanik dan pengadaan bahan baku material.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>(2)</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Untuk melaksanakan Tugas, UPTD Perlengkapan dan Perbekalan mempunyai Fungsi :</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>a.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Penyusunan rencana teknis operasional Perlengkapan dan Perbekalan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>b.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Penyiapan bahan rumusan kebijakan dibidang Perlengkapan dan Perbekalan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>c.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Penyusunan petunjuk teknis dan pedoman standar Perlengkapan dan Perbekalan</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>d.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Penyusunan rencana kebutuhan Perlengkapan dan Perbekalan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>e.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Pelaksanaan pengadaan pengelolaan bahan baku material (logistik) pendukung kegiatan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>f.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Pelaksanaan pengelolaan, pengoperasian, dan pemeliharaan Peralatan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>g.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Penginventarisasian dan pendataan terhadap Perlengkapan dan Perbekalan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>h.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Pelaksanaan pengendalian, perawatan dan perbaikan peralatan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>i.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Pelaksanaan evaluasi dan monitoring pelaksanaan kegiatan.</p></td>
+                </tr>
+
+                <tr>
+                    <td valign="top"><strong>j.</strong></td>
+                    <td align="left">
+                        <p style="text-align: justify">Penyusunan dan penyampaian laporan pelaksanaan kegiatan.</p></td>
+                </tr>
+
                  <tr>
                     <td align="center"></td>
                     <td><strong>Dasar Hukum</strong></td>
