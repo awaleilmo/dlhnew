e5af45d60cd2bd6760b3fc3043e441c7dbd641bc
Awaludin Ramdani <50146172+awaleilmo@users.noreply.github.com>
2019-12-19 21:10:29 +0700

    bodoah

--- resources/views/program/banksampah.blade.php
+++ resources/views/program/banksampah.blade.php
@@ -52,9 +52,89 @@
                     <td style="">Pemilah/pengepul</td>
                 </tr>
 
+                <tr>
+                    <td align="center">
+                        <img src="image-resources/dlh/mendirikan.png" style="width:80%; height:70%">
+                    </td>
+                </tr>
 
+                <tr>
+                    <td align="center">
+                        <img src="image-resources/dlh/menjadi.png" style="width:80%; height:70%">
+                    </td>
+                </tr>
 
+                <tr>
+                    <td colspan="2" style=""><strong style="text-decoration:underline">Partisipasi Masyarakat dalam Pengelolaan Bank Sampah</strong></td>
+                </tr>
+                <tr>
+                    <td style=""><strong>1.</strong></td>
+                    <td style="">Saat ini jumlah Bank Sampah di Kota Serang mencapai 22 Bank Sampah dengan jumlah nasabah yang cenderung meningkat dari Tahun ke Tahun.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>2.</strong></td>
+                    <td style="">Motif paling dominan yang melatarbelakangi pendirian Bank Sampah dan menjadi  nasabah Bank Sampah adalah ingin turut serta dalam menjaga lingkungan.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>3.</strong></td>
+                    <td style="">Pengelola Bank Sampah masih membutuhkan pelatihan yang bersifat manajemen dan keterampilan terkait dengan pengelolaan sampah.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>4.</strong></td>
+                    <td style="">Pencatatan terkait dengan aktifitas penabungan sampah di Bank Sampah masih sederhana dan perlu dioptimalkan lagi menjadi kegiatan terstandarkan.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>5.</strong></td>
+                    <td style="">Kegiatan pengolahan sampah organik menjadi kompos dan biogas dan membuat handicraft dari sampah yang dilakukan oleh para nasabah Bank Sampah.</td>
+                </tr>
 
+                <tr>
+                    <td colspan="2" style=""><strong style="text-decoration:underline">Hambatan Bank Sampah</strong></td>
+                </tr>
+                <tr>
+                    <td style=""><strong>1.</strong></td>
+                    <td style="">Keterbatasan tempat, bangunan atau lahan.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>2.</strong></td>
+                    <td style="">Keterbatasan sarana timbangan, alat angkut sampah dan alat pengolah sampah.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>3.</strong></td>
+                    <td style="">Keterbatasan ilmu dan kemampuan manajemen.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>4.</strong></td>
+                    <td style="">Naik turunnya semangat menjadi nasabah.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>5.</strong></td>
+                    <td style="">Kurangnya tenaga laki-laki.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>6.</strong></td>
+                    <td style="">Masih adanya masyarakat yang tidak peduli  terhadap.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>7.</strong></td>
+                    <td style="">Sulitnya mencari pengurus.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>8.</strong></td>
+                    <td style="">Sulitnya mendapatkan nasabah.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>9.</strong></td>
+                    <td style="">Kurangnya dukungan dalam pengelolaan.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>10.</strong></td>
+                    <td style="">Perlunya modal awal pendirian.</td>
+                </tr>
+                <tr>
+                    <td style=""><strong>11.</strong></td>
+                    <td style="">Perlunya honor bagi pengurus.</td>
+                </tr>
 
                 </tbody>
             </table>
