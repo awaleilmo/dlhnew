c6a4c8610cd0978ec1935bb42187ceae91e281c2
erniku sayang <awal.eilmmo@gmail.com>
2019-12-25 19:10:26 +0700

    tes

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -9,33 +9,13 @@
                 <br>
                 <tbody>
                 <tr>
-                    <td style="width: 27px;" valign="top">
-                        <strong>Perwal</strong>
-                    </td>
-                </tr>
-                <tr>
                     <td style="width: 976px;">
-                        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p style="text-align: justify">Data Informasi Publik</p>
                     </td>
                 </tr>
                 <tr>
                     <td style="width: 27px;" valign="top">
                         <strong>SOP</strong>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width: 976px;">
-                        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width: 27px;" valign="top">
-                        <strong>Dasar Hukum</strong>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="width: 976px;">
-                        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                     </td>
                 </tr>
                 </tbody>
