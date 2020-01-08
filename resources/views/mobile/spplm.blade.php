a40c36c7b838eff5e91337ca7f05a812701fc604
awal.eilmo@gmail.com <awal.eilmo@gmail.com>
2020-01-08 21:40:54 +0700

    all new

--- resources/views/mobile/spplm.blade.php
+++ resources/views/mobile/spplm.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="hero-box bg-white hero-box-smaller ">
         <div class="container">
-            <h2 class="hero-heading wow fadeInDown animated animated" data-wow-duration="0.6s" style="visibility: visible; animation-duration: 0.6s;">SPPL</h2>
             <table border="0" cellpadding="10" class="hero-text wow bounceInUp animated animated" data-wow-duration="0.9s" data-wow-delay="0.2s" style="text-align: justify; visibility: visible; animation-duration: 0.9s; animation-delay: 0.2s;">
                 <br>
                 <br>
