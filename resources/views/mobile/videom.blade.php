52f2b41044c6daed881256cd1c09ac34180882d8
awal.eilmo@gmail.com <awal.eilmo@gmail.com>
2020-03-15 22:07:08 +0700

    fixed

--- resources/views/mobile/videom.blade.php
+++ resources/views/mobile/videom.blade.php
@@ -7,7 +7,7 @@
 
                     <div style="margin-left: 5px; margin-right: 5px;">
                         <div class="panel panel-info" style="margin-left: 10px; margin-right: 10px">
-                            <div class="panel-heading text-uppercase"> {{$p->judul}} </div>
+                            <div class="panel-heading text-uppercase"><h1>{{$p->judul}} </h1></div>
                             <div id="fotol{{$p->id}}" class="panel-body">
                                 <iframe width="100%" height="600px" src="https://www.youtube.com/embed/{{$p->link}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
